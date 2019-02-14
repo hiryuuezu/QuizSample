db4daa00ffa460191fd8cfa185471008d5c01da0
上江洲飛龍 <hiryu2088@gmail.com>
2019-02-14 21:37:36 +0900

    すくりぷとまとめたよ

--- Assets/judgeManager.cs
+++ Assets/judgeManager.cs
@@ -5,17 +5,12 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class JudgeManager : MonoBehaviour {
-    int quizCount;
-    int trueCount;
-    QuizManager script;
-    GameObject quizM;
+
    
 
     // Use this for initialization
     void Start () {
-        quizCount = 0;
-        quizM = GameObject.Find("QuizM");
-        script = quizM.GetComponent<QuizManager>();
+
     }
 
     // Update is called once per frame
@@ -24,111 +19,10 @@ public class JudgeManager : MonoBehaviour {
 
 
 
-        switch (quizCount)
-        {
-            case 1:
-                script.Quetion2();
-                break;
-            case 2:
-                script.Quetion3();
-                break;
-            case 3:
-                PlayerPrefs.SetInt("count", trueCount);
-                SceneManager.LoadScene("EndScene");
-                break;
-
-
-
-        }
-
+       
 
 
     }
-    public void QuetionAnswer1()
-    {
-       Text selectedBtn = GameObject.Find("Button1").GetComponentInChildren<Text>();
 
-
-        if (selectedBtn.text == "2020年" || selectedBtn.text == "野球" || selectedBtn.text == "カレー")
-        {
-
-            trueCount++;
-            Debug.Log("正解");
-
-        }
-        else
-        {
-            Debug.Log("不正解");
-
-        }
-
-        quizCount++;
-        Debug.Log(quizCount);
-    }
-
-    public void QuetionAnswer2()
-    {
-      Text  selectedBtn = GameObject.Find("Button2").GetComponentInChildren<Text>();
-
-
-        if (selectedBtn.text == "2020年" || selectedBtn.text == "野球" || selectedBtn.text == "カレー" )
-        {
-
-            trueCount++;
-            Debug.Log("正解");
-
-        }
-        else
-        {
-            Debug.Log("不正解");
-           
-        }
-
-        quizCount++;
-        Debug.Log(quizCount);
-    }
-    public void QuetionAnswer3()
-    {
-      Text  selectedBtn = GameObject.Find("Button3").GetComponentInChildren<Text>();
-
-
-        if (selectedBtn.text == "2020年" || selectedBtn.text == "野球" || selectedBtn.text == "カレー")
-        {
-
-            trueCount++;
-            Debug.Log("正解");
-
-        }
-        else
-        {
-            Debug.Log("不正解");
-
-        }
-
-        quizCount++;
-        Debug.Log(quizCount);
-    }
-
-    public void QuetionAnswer4()
-    {
-      Text  selectedBtn = GameObject.Find("Button4").GetComponentInChildren<Text>();
-
-
-        if (selectedBtn.text == "2020年" || selectedBtn.text == "野球" || selectedBtn.text == "カレー")
-        {
-
-            trueCount++;
-            Debug.Log("正解");
-
-        }
-        else
-        {
-            Debug.Log("不正解");
-
-        }
-
-        quizCount++;
-        Debug.Log(quizCount);
-    }
 }
 
--- Assets/quizManager.cs
+++ Assets/quizManager.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QuizManager : MonoBehaviour
 {
     string[,] quizDate;
     public Text buttonText;
     public Text text;
+    int quizCount;
+    int trueCount;
+    GameObject quizM;
+    //基本理念：くりかえしはさける
+    //=>もしなんどもこぴぺしているなとかんじたら、まとめられないか考える。
+    //つねにインデントは揃える => {}の位置がわからなくなる。
+    //空白行も無駄に増やさない。
 
 
 
     // Use this for initialization
     void Start()
     {
+        quizCount = 0;
 
-
+        quizM = GameObject.Find("QuizM");
         Quetion1();
 
     }
@@ -23,12 +32,22 @@ public class QuizManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-
+        switch (quizCount)
+        {
+            case 1:
+                Quetion2();
+                break;
+            case 2:
+                Quetion3();
+                break;
+            case 3:
+                PlayerPrefs.SetInt("count", trueCount);
+                SceneManager.LoadScene("EndScene");
+                break;
+        }
     }
 
-   public void Quetion1()
+    public void Quetion1()
     {
         quizDate = csvManager.ReadCsvFile("CSV/quiz");
         text.text = quizDate[1, 0];
@@ -37,7 +56,6 @@ public class QuizManager : MonoBehaviour
             buttonText = GameObject.Find("Button" + i).GetComponentInChildren<Text>();
 
             buttonText.text = quizDate[1, i];
-
         }
 
     
@@ -66,5 +84,92 @@ public class QuizManager : MonoBehaviour
             buttonText.text = quizDate[3, i];
 
         }
+    }
+
+    public void QuetionAnswer1()
+    {
+        Text selectedBtn = GameObject.Find("Button1").GetComponentInChildren<Text>();
+
+
+        if (selectedBtn.text == "2020年" || selectedBtn.text == "野球" || selectedBtn.text == "カレー")
+        {
+
+            trueCount++;
+            Debug.Log("正解");
+
+        }
+        else
+        {
+            Debug.Log("不正解");
+
+        }
+
+        quizCount++;
+        Debug.Log(quizCount);
+    }
+
+    public void QuetionAnswer2()
+    {
+        Text selectedBtn = GameObject.Find("Button2").GetComponentInChildren<Text>();
+
+
+        if (selectedBtn.text == "2020年" || selectedBtn.text == "野球" || selectedBtn.text == "カレー")
+        {
+
+            trueCount++;
+            Debug.Log("正解");
+
+        }
+        else
+        {
+            Debug.Log("不正解");
+
+        }
+
+        quizCount++;
+        Debug.Log(quizCount);
+    }
+    public void QuetionAnswer3()
+    {
+        Text selectedBtn = GameObject.Find("Button3").GetComponentInChildren<Text>();
+
+
+        if (selectedBtn.text == "2020年" || selectedBtn.text == "野球" || selectedBtn.text == "カレー")
+        {
+
+            trueCount++;
+            Debug.Log("正解");
+
+        }
+        else
+        {
+            Debug.Log("不正解");
+
+        }
+
+        quizCount++;
+        Debug.Log(quizCount);
+    }
+
+    public void QuetionAnswer4()
+    {
+        Text selectedBtn = GameObject.Find("Button4").GetComponentInChildren<Text>();
+
+
+        if (selectedBtn.text == "2020年" || selectedBtn.text == "野球" || selectedBtn.text == "カレー")
+        {
+
+            trueCount++;
+            Debug.Log("正解");
+
+        }
+        else
+        {
+            Debug.Log("不正解");
+
+        }
+
+        quizCount++;
+        Debug.Log(quizCount);
     }
 }
